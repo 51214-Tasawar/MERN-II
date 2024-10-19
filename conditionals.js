8e781315b4a361c38fc70db8cf627104339eeefa
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-19 11:52:29 +0500

    Test

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,6 @@
-TotalNum = 57 + 97 + 75 + 99 ;
+TotalNum = 57 + 97 + 599 + 99 ;
 percentage = TotalNum / 400 * 100;
-if(percentage >= 85){
+if(percentage >= 85 && percentage<=100){
     a = "Passed"
     console.log(a)
 }
@@ -8,3 +8,16 @@ else{
     a = "Try Again"
     console.log(a)
 }
+switch(a){
+case "Passed" :
+    console.log("Your Eligible for this Job");
+    break;
+    case "Try Again":
+        console.log("Please Try Again Next Time");
+        break ;
+        default :
+        console.log("Invalid Input");
+        break;
+}
+const b= a>= 85 ?  "Please Fill out this Form":  "Your not able not able fill out this form";
+ console.log(b)
