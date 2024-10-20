3eac424c70f49ef49e2040595e0ca1f97176080c
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-20 09:41:57 +0500

    Testing Demo

---
+++ objects.js
@@ -0,0 +1,11 @@
+const Obj1 = {
+    name : "Person1",
+    age : 40 ,
+    add : {
+        city : "MuzaffarGarh" ,
+        provinace : "Punjab" ,
+        Country : "Pakistan"
+    }
+}
+
+console.log(Obj1.add)
