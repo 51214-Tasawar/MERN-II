4a42203982e49a37bb07a5802cac9e01c9b6beb3
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-22 08:54:27 +0500

    Update

--- objects.js
+++ objects.js
@@ -1,11 +1,18 @@
-const Obj1 = {
-    name : "Person1",
-    age : 40 ,
-    add : {
-        city : "MuzaffarGarh" ,
-        provinace : "Punjab" ,
-        Country : "Pakistan"
+const Info ={
+    name :"Person1",
+    age : 40  ,
+    desination:"employee",
+    add:{
+        city:"Lahore",
+        Provinace :"Punjab",
+        Country:"Pakistan"
     }
 }
-
-console.log(Obj1.add)
+const Obj2 = Info ; // Object also create Shallow Copy Just Like Array 
+// console.log(Obj2)
+//  Obj2.age = 45;
+//  console.log(Obj2)
+//  console.log(Info)
+ const {name , age , add:{city}} = Obj2// Desturcturing of Object and object with-in Object 
+ console.log(`My Name is ${name} and I am ${age} Year Old `)
+ console.log(`I am living in ${city} City`)
