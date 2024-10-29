a34b586cea45fd5acdac43eb743af92acb744d27
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-29 20:59:09 +0500

    Goes To Update

--- Functions/simplefunction.js
+++ Functions/simplefunction.js
@@ -6,8 +6,16 @@
 
 // <----------- Function with Parmeters  ------->
 
-function info(name , age  , address){
-  console.log(`My name is ${name} . I am ${age} Year Old . I live in ${address}`);
+// function info(name , age  , address){
+//   console.log(`My name is ${name} . I am ${age} Year Old . I live in ${address}`);
+// }
+
+// info("Person1" , 20 , "Lahore")
+
+// <------------- Return Function ---------->
+
+function addition(a , b , c){
+return console.log(`Total marks are ${a + b + c} out 300 .`)
 }
 
-info("Person1" , 20 , "Lahore")
+addition(12,34,56);
