52a2ff9f029cee5480807c7f55b4d5ef3495aabb
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-31 20:45:52 +0500

    Update

--- Loops/forloop.js
+++ Loops/forloop.js
@@ -1,4 +1,10 @@
 const a = 20 ;
-for(i=0 ; i<=10; i++){
+i=0
+// for(i; i<=10; i++){   // For Loop
+//     console.log(`${a} * ${i} = ${a*i}`)
+// }
+
+while (i<=10) {
     console.log(`${a} * ${i} = ${a*i}`)
+    i++ ;
 }
