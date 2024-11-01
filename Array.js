3c71322e0825e565114426b215e485bd8dd85839
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-01 19:44:10 +0500

    Testing shift method

--- Array.js
+++ Array.js
@@ -2,5 +2,6 @@ const arr = [1 , undefined , null , "Person1" , "Aquwa"] ;
 
 const [a ,b,...e]=arr // DESTRUCTURING OF ARRAY
 console.log(e) ;
-arr.sort(); // sorting method
+// arr.sort(); // sorting method
+arr.shift();  // remove the frist value of array
 console.log(arr);
